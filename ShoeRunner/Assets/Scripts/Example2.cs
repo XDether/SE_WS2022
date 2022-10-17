682424a4fe8f361148be1760119674e8102c5ccb
Jacek Bannach <jacek_b_@hotmail.com>
2022-10-17 20:20:32 +0200

    testing

--- ShoeRunner/Assets/Scripts/Example2.cs
+++ ShoeRunner/Assets/Scripts/Example2.cs
@@ -7,7 +7,7 @@ public class Example2 : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Somthing new");
+        Debug.Log("Somthing new yeah");
         
     }
 
