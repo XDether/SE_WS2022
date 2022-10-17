d018672cd6ec3f53f45b621699bdb2e9e3a89e62
Jacek Bannach <jacek_b_@hotmail.com>
2022-10-17 20:11:31 +0200

    testing

--- ShoeRunner/Assets/Scripts/Example2.cs
+++ ShoeRunner/Assets/Scripts/Example2.cs
@@ -7,7 +7,7 @@ public class Example2 : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("this is a diffrent branch");
     }
 
     // Update is called once per frame
